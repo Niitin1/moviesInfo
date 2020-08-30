f5a86d62ecc0f434c5a13945216a5e232865e80e
Nitin <nksinghal41@gmail.com>
2020-08-30 10:09:48 +0530

    log file

--- src/main/java/io/nitinLearn/moviesinfoservice/controller/HomeController.java
+++ src/main/java/io/nitinLearn/moviesinfoservice/controller/HomeController.java
@@ -1,10 +1,20 @@
 package io.nitinLearn.moviesinfoservice.controller;
 
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.nitinLearn.moviesinfoservice.service.HomeService;
+
+
 @RestController
 public class HomeController {
+	
+	@Autowired
+	HomeService homeService;
+	
+	private final Logger log = Logger.getLogger(this.getClass());
 	
 	@GetMapping(value = "/")
 	public String getTest() {
---
+++ src/main/java/io/nitinLearn/moviesinfoservice/service/HomeService.java
@@ -0,0 +1,8 @@
+package io.nitinLearn.moviesinfoservice.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HomeService {
+
+}
