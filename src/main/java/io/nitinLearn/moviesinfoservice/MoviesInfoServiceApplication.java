93ce889ca8a48c6aa3dfb9a68396c33caecc874f
NitinSP <nitin@srslive.in>
2020-09-02 11:41:13 +0530

    register eureka service , changes made in properties file , set listening port and application name

--- src/main/java/io/nitinLearn/moviesinfoservice/MoviesInfoServiceApplication.java
+++ src/main/java/io/nitinLearn/moviesinfoservice/MoviesInfoServiceApplication.java
@@ -2,10 +2,11 @@ package io.nitinLearn.moviesinfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class MoviesInfoServiceApplication {
 
 	public static void main(String[] args) {
